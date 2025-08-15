4d72545cde68d6238e53eaf8937c893065e2d069
AdamKuzniarski <adam.kuzniarski@gmx.de>
2025-08-15 13:59:20 +0200

    add API-create

--- pages/api/places/index.js
+++ pages/api/places/index.js
@@ -8,10 +8,16 @@ export default async function handler(request, response) {
     const places = await Place.find();
 
     if (!places) {
-      response.status(404).json({ status: "Not found" });
-      return;
+      return response.status(404).json({ status: "Not found" });
     }
     return response.status(200).json(places);
+  }
+
+  if (request.method === "POST") {
+    const placeData = request.body;
+    await Place.create(placeData);
+
+    return response.status(201).json({ status: "Place created" });
   }
 
   response.status(405).json({ status: "Method not Allowed" });
